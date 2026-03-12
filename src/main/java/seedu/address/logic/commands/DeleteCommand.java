815d3b4279aa3e588b383c910f354542e1d4dfd2
Ruben <xcreeper0511@gmail.com>
2026-03-12 23:33:03 +0800

    Fix new test conds

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
